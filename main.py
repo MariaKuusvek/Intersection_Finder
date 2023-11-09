3921e97851ada16713559e0d927abf8b96cee2cd
Kristjan K6iv <kristjankv32@gmail.com>
2023-11-09 19:33:05 +0200

    asd

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import json
+import utm
+import glob
 
 
 def main():
-    file = open("riigiteed_xgis.geojson", "r")
+    file = open("../riigiteed_xgis.geojson", "r")
     data = file.read()
     file.close()
     data = json.loads(data)["features"]
     data = [x["geometry"] for x in data]
+
     linestrings = []
     multilinestrings = []
     for a in data:
@@ -14,6 +17,13 @@ def main():
             linestrings.append(a["coordinates"])
         else:
             multilinestrings.append(a["coordinates"])
+    
+    for filename in glob.glob("sample_maps_zipped/map_gif_1km/*"):
+        coords = filename.split("/")[-1].split(".")[0].split("_")[1:]
+        x, y = int(coords[0]), int(coords[1])
+        print(coords, filename)
+    return
+
     print(len(linestrings))
     print(len(multilinestrings))
     print(linestrings[0])
