991c94a25508abc9d81fe91111606e686c3a9a73
Kristjan K6iv <kristjankv32@gmail.com>
2023-11-09 19:44:12 +0200

    asd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 import utm
 import glob
+import cv2
 
 
 def main():
@@ -21,7 +22,15 @@ def main():
     for filename in glob.glob("sample_maps_zipped/map_gif_1km/*"):
         coords = filename.split("/")[-1].split(".")[0].split("_")[1:]
         x, y = int(coords[0]), int(coords[1])
-        print(coords, filename)
+        cap = cv2.VideoCapture(filename)
+        ret, im = cap.read()
+        cap.release()
+        cv2.imshow("i", im)
+        xl, yl = utm.to_latlon(x, y, 35, 'V')
+        
+        print(coords, filename, xl, yl)
+        if cv2.waitKey() == 27:
+            break
     return
 
     print(len(linestrings))
